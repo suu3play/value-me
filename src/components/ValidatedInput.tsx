b355cc989ec07ee830a4e5daba3e16e51b521689
bstale <bstale11@gmail.com>
2025-10-29 20:59:51 +0900

    style: 数値入力欄のデフォルトスピンボタンを非表示に設定

--- src/components/ValidatedInput.tsx
+++ src/components/ValidatedInput.tsx
@@ -157,6 +157,19 @@ const ValidatedInput: React.FC<ValidatedInputProps> = ({
           onBlur={handleBlur}
           onFocus={handleFocus}
           disabled={disabled}
+          sx={{
+            '& input[type=number]': {
+              MozAppearance: 'textfield',
+            },
+            '& input[type=number]::-webkit-outer-spin-button': {
+              WebkitAppearance: 'none',
+              margin: 0,
+            },
+            '& input[type=number]::-webkit-inner-spin-button': {
+              WebkitAppearance: 'none',
+              margin: 0,
+            },
+          }}
           endAdornment={
             <InputAdornment position="end">
               {inlineMultiStepButtons && multiStepButtons && !disabled && (
